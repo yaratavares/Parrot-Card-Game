a0ed6749f796a85343eed745d13681b2320cc6d8
Yara Tavares <yaracriistavares@gmail.com>
2021-11-05 14:25:18 -0300

    Visual effect click

--- main.js
+++ main.js
@@ -1,14 +1,63 @@
+const allCards = [
+    "bobrossparrot",
+    "explodyparrot",
+    "fiestaparrot",
+    "metalparrot",
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot"
+]
 
 
+
+
+
+// função de validação do numero digitado
 function validation (){
-    let valid = prompt ("Com quantas cartas vamos jogar?");
-    
+    const valid = prompt ("Com quantas cartas vamos jogar?");
     if (valid%2 !== 0 || valid<2 || valid>14) {
         alert ("Número inserido deve ser par e entre 2 e 14");
         validation ();
     } else {
         return(valid);
     }
+} 
+
+// função de mostrar as cartas
+function showCards (numberCards){
+    
+    const listCards = document.querySelector(".container");
+    for (i=0; i < numberCards; i++) {
+        
+        listCards.innerHTML += 
+        `<div class="card" onclick="clickCard(this)">
+            <div class= "front-face face">
+                <img class="parrot" src= "assets/parrot.png"/>
+            </div>
+            <div class= "back-face face">
+                <img src="assets/bobrossparrot.gif"/>
+            </div>
+        </div>`
+    }
+    
 }
 
-numberOfCards = validation ();
+function clickCard (locationCard){
+    locationCard.classList.add('click');
+}
+// lógica implementada
+
+
+const numberOfCards = validation ();
+showCards (numberOfCards);
+
+
+
+
+// allCards.sort(comparador);
+
+// showCards();
+
+// function comparador() { 
+//     return Math.random() - 0.5; 
+// }
