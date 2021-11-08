03586bbc8a1a5fbcffcc6e6a1bed573198aaa91b
Yara Tavares <yaracriistavares@gmail.com>
2021-11-08 15:17:08 -0300

    ready

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let allCards = [
+const allCards = [
     "bobrossparrot",
     "explodyparrot",
     "fiestaparrot",
@@ -7,7 +7,6 @@ let allCards = [
     "tripletsparrot",
     "unicornparrot",
 ]
-
 let card1;
 let card2;
 let numberOfCards;
@@ -21,16 +20,58 @@ let second = 0;
 let minute = 0;
 let IdentificadorIntervalo;
 
+// FUNÇÃO QUE GOVERNA TODAS AS FUNÇÕES A SEGUIR
+function gameRepeat () {
+    // validação do número escolhido
+    numberOfCards = validation ();
+
+    if (numberOfCards !== null) {
+        // sorteando todas as cards
+        allCards.sort(comparador);
+        
+        // pegando somente as cartas que queremos
+        gameCards = gameSort();
+        
+        // sorteando a ordem das cartas novamente
+        gameCards.sort(comparador);
+        
+        // mostrando as cartas
+        showCards (numberOfCards)
+        
+        // ligando o timer (bônus)
+        timer ();
+    }
+}
+
 // função de validação do numero digitado
 function validation (){
-    const valid = prompt ("Com quantas cartas vamos jogar?");
-    if (valid%2 !== 0 || valid<2 || valid>14) {
-        alert ("Número inserido deve ser par e entre 2 e 14");
-        validation ();
+    let valid = prompt ("Com quantas cartas vamos jogar?");
+    if (valid == null){
+        return (valid);
     } else {
-        return(valid);
+        while (valid%2 !== 0 || valid<2 || valid>14) {
+            alert ("Número inserido deve ser par e entre 2 e 14");
+            valid = prompt ("Com quantas cartas vamos jogar?");
+        } 
+        return (valid);
     }
 } 
+
+// função que misturamos as cartas
+function comparador() { 
+    return Math.random() - 0.5; 
+}
+
+// pegando o número de cartas necessárias para o jogo
+function gameSort() {
+    let cardsAleatory = allCards.slice(0);
+    console.log(cardsAleatory);
+    cardsAleatory.length = numberOfCards/2;
+    for (i=0; i < numberOfCards/2; i++){
+        cardsAleatory.push(cardsAleatory[i]); 
+    } 
+    return (cardsAleatory);
+}
 
 // função de mostrar as cartas
 function showCards (numberCards){
@@ -49,40 +90,35 @@ function showCards (numberCards){
         </div>`
     }
 }
+// TODAS AS FUNÇÕES GOVERNADAS POR GAMEREPEAT TERMINA AQUI
 
-// função que misturamos as cartas
-function comparador() { 
-    return Math.random() - 0.5; 
-}
-
-// pegando o número de cartas necessárias para o jogo
-function gameSort() {
-    allCards.length = numberOfCards/2;
-    for (i=0; i < numberOfCards/2; i++){
-        allCards.push(allCards[i]); 
-    } 
-    return (allCards);
-}
-
-// função do clique das cartas
+// FUNÇÕES DO EVENTO CLICAR
 function clickCard (locationCard){
     locationCard.classList.add('click');
     contClick++;
+
     let identificationCard = locationCard.lastElementChild;
     
     gameClick(identificationCard);
 }
 
 function gameClick (identificationCard) {
-    if (testGame < 1){
+
+    if (testGame == 0){
         card1 = identificationCard;
+        (card1.parentElement).classList.add('noClick');
         testGame++;
      } else {
+        blockEveryone();
         card2 = identificationCard;
         testGame = 0;
         if (card1.innerHTML !== card2.innerHTML){
             setTimeout (removeProgram, 1000);
+           
         } else {
+            (card1.parentElement).classList.add('acertou');
+            (card2.parentElement).classList.add('acertou');
+            removeProgram();
             contGame += 2;
         }
      }
@@ -95,14 +131,53 @@ function gameClick (identificationCard) {
 function removeProgram (){
     (card1.parentElement).classList.remove('click');
     (card2.parentElement).classList.remove('click');
+    unlockEveryone();
 }
+// solução implementada para impedir cliques sucessivos nos cards
+function blockEveryone (){
+    const noBug = document.querySelectorAll('.card');
+ 
+    for (let i=0; i < numberOfCards; i++){
+        (noBug[i]).classList.add('noClick');
+    }
+}
+function unlockEveryone (){
+    const noBug = document.querySelectorAll('.card');
+ 
+    for (let i=0; i < numberOfCards; i++){
+        (noBug[i]).classList.remove('noClick');
+    }
+}
+// FUNÇÕES DESENCADEADAS PELO EVENTO CLICAR FINALIZAM AQUI
 
-//responsável pelo alert ao final do jogo
+// FUNÇÃO RESPONSÁVEL PELO ALERT AO FINAL DO JOGO
 function endOfGame (contClick) {
     alert(`Você ganhou em ${contClick} rodadas! Em ${minute} minutos e ${second} segundos`);
+    clearInterval(IdentificadorIntervalo);
+    
+    // PARTE RESPONSÁVEL PELA REPETIÇÃO SUCESSIVA DO JOGO (BÔNUS)
+    let choise = prompt ("Você quer reiniciar o jogo? Digite 'sim' para continuar a jogar e 'não' para sair.");
+    if (choise == "sim") {
+        second = 0;
+        minute = 0;
+
+        var node = document.querySelectorAll('.card');
+        
+        for (i=0; i<numberOfCards; i++){
+            node[i].parentNode.removeChild(node[i]);
+        }
+        
+        contGame = 0;
+        testGame = 0;
+        contClick= 0;
+        gameCards = [];
+        gameRepeat()
+    } else {
+        return null;
+    }
 }
 
-// função do contador (bônus)
+// FUNÇÃO PARA O CONTATOR (BÔNUS)
 function timer() {
     IdentificadorIntervalo = setInterval(counter, 1000);
 }
@@ -127,31 +202,6 @@ function counter() {
     }
 }
 
+gameRepeat ();
 
-// lógica implementada
-numberOfCards = validation ();
-console.log(numberOfCards);
-    
-// sorteando todas as cardas
-allCards.sort(comparador);
-    
-// pegando somente as cartas que queremos
-gameCards = gameSort();
-
-// sorteando a ordem das cartas novamente
-gameCards.sort(comparador);
-
-// mostrando as cartas
-showCards (numberOfCards);
-
-// inicia timer (bônus)
-timer ();
-
-
-
-
-   
-
-
-
-
+// ALTERAÇÃO FUTURA: leve delay no timer
